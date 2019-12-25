5686af13841940eacc76e6a2a8da9bf882d05ff6
Frank J <44736321+ItsTheBox@users.noreply.github.com>
2019-12-25 03:31:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const Discord = require('discord.js'); //Things to make Discord integration work.
+const Discord = require('discord.js'); //Require discord.js so bot can function and integrate with Discord services.
 const client = new Discord.Client(); 
 const token = ; //Token to allow project to log into bot
-const PREFIX = '%'; //Command prefix.
-var version = "0.0.8"
+const config = require("./config.json");
+var version = "0.1.0"
 
 client.login(token); //Login to Discord services.
 
 client.on('ready', () =>{
-     console.log('Putting on Cargo Shorts...'); //Ensure bot is functional and online
+    console.log(`DadBot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); //Ensure bot is functional and online
 
-client.user.setActivity('DadBot Development', {type: "WATCHING"}).catch(console.error);
+    client.user.setActivity(`Being a dad in ${client.guilds.size} server(s)!`);
 
-//Begin welcome listener.
+
+
+//Welcome bot
 
 client.on('guildMemberAdd', member =>{
     const channel = member.guild.channels.find(ch => ch.name === 'auto-gen-spam');
@@ -20,27 +22,122 @@ client.on('guildMemberAdd', member =>{
 });
 })
 
-client.on('message', msg=>{ //Basic commands for bot.
+  
+client.on("guildCreate", guild => {
+    
+    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+    client.user.setActivity(`Being a dad in ${client.guilds.size} servers(s)!`);
+  });
+  
+  client.on("guildDelete", guild => {
+    
+    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+    client.user.setActivity(`Being a dad in ${client.guilds.size} servers(s)!`);
+  });
+  
+  
+  client.on("message", async message => {
    
-    let args = msg.content.substring(PREFIX.length).split(" ");
+    
+ 
+    if(message.author.bot) return;
+    
+  
+    if(message.content.indexOf(config.prefix) !== 0) return;
+    
 
-    switch(args[0]){
-        case 'ping':
-            msg.channel.send('Go ask your mother.')
-            break;
-        case 'website':
-            msg.channel.send('As requested, the website for my creator is itsthebox.net :)')
-            break;
-        case 'info':
-            if(args[1] === 'version'){
-                msg.channel.send('DadBot is currently on version ' + (version))
-            }else{
-                msg.channel.send('I am unable to process that request :( Please make sure you provide the correct argument (info, version, etc)')
-            }
-            break;
-        case 'clear':
-            if(!args[1]) return msg.reply('please specify the number of messages you would like to be removed.')
-            msg.channel.bulkDelete(args[1]);
-            break;
-        }
-    })
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    //Info
+
+    if(command == "info"){
+        return message.reply("Hi! I'm DadBot :man: Currently, I am running on DadBot version " + (version) + ". If you would like to contact my creator, please email frank@itsthebox.net :envelope:" )
+    }
+
+    //Help
+
+    if (command == "help"){
+        return message.reply("You can use %command to invoke commands. Administration commands include %kick, %ban, and %purge. You muse have the proper permissions to use these commands! For a list of commands please see _inset Github here_.")
+    }
+
+    
+   //Ping
+    
+    if(command === "ping") {
+
+      const m = await message.channel.send("Ping?");
+      m.edit(`Complete! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    }
+    
+    if(command === "say") {
+   
+      const sayMessage = args.join(" ");
+
+      message.delete().catch(O_o=>{}); 
+    
+      message.channel.send(sayMessage);
+    }
+    
+    if(command === "kick") {
+      
+      if(!message.member.roles.some(r=>["Frank"].includes(r.name)) )
+        return message.reply("you don't have permission to kick people!");
+      
+    
+      let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+      if(!member)
+        return message.reply("Please mention a valid member of this server");
+      if(!member.kickable) 
+        return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+      
+     
+      let reason = args.slice(1).join(' ');
+      if(!reason) reason = "No reason provided";
+      
+     
+      await member.kick(reason)
+        .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+      message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+  
+    }
+    
+
+    // Can use ["Administrator", "Moderator"] for multiple roles
+    if(command === "ban") {
+     
+      if(!message.member.roles.some(r=>["Frank"].includes(r.name)) )
+        return message.reply("you don't have permission to ban people!");
+      
+      let member = message.mentions.members.first();
+      if(!member)
+        return message.reply("Please mention a valid member of this server");
+      if(!member.bannable) 
+        return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+  
+      let reason = args.slice(1).join(' ');
+      if(!reason) reason = "No reason provided";
+      
+      await member.ban(reason)
+        .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+      message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+    }
+    
+    //Purging Messages
+
+    if(command === "purge") {
+    
+    if(!message.member.roles.some(r=>["Frank"].includes(r.name)) )
+    return message.reply("you don't have permission to purge messages!");
+      
+      const deleteCount = parseInt(args[0], 10);
+      
+      
+      if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+        return message.reply("please provide a number between 2 and 100 for the number of messages to delete");
+      
+      const fetched = await message.channel.fetchMessages({limit: deleteCount});
+      message.channel.bulkDelete(fetched)
+        .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+    }
+  });
